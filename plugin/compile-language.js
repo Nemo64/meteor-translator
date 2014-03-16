465827c98b637441ea3f13c8a67570f5b4cdad75
Marco Pfeiffer <cheat2000-git@yahoo.de>
2014-03-16 16:17:58 +0100

    keys are now joined in the plugin

--- plugin/compile-language.js
+++ plugin/compile-language.js
@@ -1,14 +1,35 @@
 var yaml = Npm.require("js-yaml");
 
+/**
+ * @param {string} baseKey
+ * @param {mixed}  value
+ * @param {Object.<string, string>} result
+ */
+var parseValue = function (baseKey, value, result) {
+  if (_.isObject(value) && ! _.isArray(value)) {
+    _.each(value, function (value, key) {
+      parseValue(baseKey + "." + key, value, result);
+    });
+  } else {
+    result[baseKey] = value;
+  }
+}
+
 var handler = function (compileStep, isLiterate) {
   var source = compileStep.read().toString("utf8");
   try {
-  
-    // parse the language file
     var doc = yaml.safeLoad(source);
+    
+    // parse the document
+    var parsedDoc = {};
+    _.each(doc, function (value, key) {
+      parseValue(key, value, parsedDoc);
+    });
+    
+    // save the file asset
     compileStep.addAsset({
       path: compileStep.inputPath + ".json",
-      data: new Buffer(JSON.stringify(doc))
+      data: new Buffer(JSON.stringify(parsedDoc))
     });
     
   } catch (e) {
