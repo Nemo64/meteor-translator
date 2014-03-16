1b98762745dcc0ff12b4fb3799c173437d05b2e2
Marco Pfeiffer <cheat2000-git@yahoo.de>
2014-03-16 13:43:43 +0100

    used the meteor-helpers better for reactive responses

--- tests/Namespace.js
+++ tests/Namespace.js
@@ -6,7 +6,8 @@ var LANGUAGE_DEFAULT = new Translator.LanguageArray();
 Tinytest.add("Namespace - correct path", function (test) {
   var namespace = new Translator.Namespace(NAMESPACE);
   var filename = namespace._filenameForLocale("de_DE");
-  test.equal(filename, "/" + NAMESPACE + ".de_DE.lang.yml.json");
+  var expect = "/" + NAMESPACE + ".de_DE.lang.yml.json";
+  test.equal(filename, expect, "the language and format should be appended");
 });
 
 testAsyncMulti("Namespace - check existence of an existing key", [
@@ -16,8 +17,7 @@ testAsyncMulti("Namespace - check existence of an existing key", [
     Deps.autorun(function (dep) {
       var result = namespace.has("a_key", LANGUAGE_FOREIGN);
       if (! namespace.isLoading()) {
-        test.isTrue(result);
-        expect(true);
+        expect(result);
       }
     });
   }
@@ -26,12 +26,11 @@ testAsyncMulti("Namespace - check existence of an existing key", [
 testAsyncMulti("Namespace - check existence of a none existing key", [
   function (test, expect) {
     var namespace = new Translator.Namespace(NAMESPACE);
-    var expect = expect(true);
+    var expect = expect(false);
     Deps.autorun(function (dep) {
       var result = namespace.has("a_key", LANGUAGE_FOREIGN);
       if (! namespace.isLoading()) {
-        test.isFalse(result);
-        expect(true);
+        expect(result);
       }
     });
   }
@@ -40,12 +39,11 @@ testAsyncMulti("Namespace - check existence of a none existing key", [
 testAsyncMulti("Namespace - access existing key", [
   function (test, expect) {
     var namespace = new Translator.Namespace(NAMESPACE);
-    var expect = expect(true);
+    var expect = expect("Hallo Test");
     Deps.autorun(function (dep) {
       var result = namespace.has("a_key", LANGUAGE_FOREIGN);
       if (! namespace.isLoading()) {
-        test.equal(result, "Hallo Test");
-        expect(true);
+        expect(result);
       }
     });
   }
@@ -54,12 +52,11 @@ testAsyncMulti("Namespace - access existing key", [
 testAsyncMulti("Namespace - access none existing key", [
   function (test, expect) {
     var namespace = new Translator.Namespace(NAMESPACE);
-    var expect = expect(true);
+    var expect = expect(undefined);
     Deps.autorun(function (dep) {
       var result = namespace.has("a_key", LANGUAGE_FOREIGN);
       if (! namespace.isLoading()) {
-        test.equal(result, undefined);
-        expect(true);
+        expect(result);
       }
     });
   }
