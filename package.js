5022858f1d506ec14ccfae8430ef0298d56d41c6
Marco Pfeiffer <cheat2000-git@yahoo.de>
2014-04-17 00:07:42 +0200

    restructoring of the app

--- package.js
+++ package.js
@@ -6,9 +6,9 @@ Package._transitional_registerBuildPlugin({
   name: 'compileLanguage',
   use: ['underscore', 'ejson'],
   sources: [
-    'lib/Locale.js',
-    'lib/LanguageArray.js',
-    'plugin/compile-language.js'
+    'src/Locale.js',
+    'src/LanguageArray.js',
+    'src/plugin/compile-language.js'
   ],
   npmDependencies: { 'js-yaml': '3.0.2' }
 });
@@ -26,23 +26,23 @@ Package.on_use(function(api) {
   ], 'client');
 
   api.add_files([
-    'lib/Locale.js',
-    'lib/LanguageArray.js',
-    'lib/Namespace.js',
-    'lib/FilterList.js',
-    'lib/Translator.js',
-    'lib/FilterList/parameter.js',
-    'lib/Translator/globalLang.js',
-    'lib/Translator/defaultLanguage.js'
+    'src/Locale.js',
+    'src/LanguageArray.js',
+    'src/Namespace.js',
+    'src/FilterList.js',
+    'src/Translator.js',
+    'src/FilterList/parameter.js',
+    'src/Translator/globalLang.js',
+    'src/Translator/defaultLanguage.js'
   ]);
   api.add_files([
-    'client/Namespace.js',
-    'client/helper.js'
+    'src/Namespace-client.js',
+    'src/helper-client.js'
   ], 'client');
   api.add_files([
-    'server/Namespace.js',
-    'server/inject/namespaces.js',
-    'server/inject/accept-language.js'
+    'src/Namespace-server.js',
+    'src/inject/namespaces-server.js',
+    'src/inject/accept-language-server.js'
   ], 'server');
   
   api.add_files(['Translator.js']);
@@ -56,14 +56,14 @@ Package.on_test(function (api) {
     'test/namespace.en_US.lang.yml'
   ]);
   api.add_files([
-    'lib/Locale-test.js',
-    'lib/LanguageArray-test.js',
-    'lib/Namespace-test.js',
-    'lib/FilterList-test.js',
-    'lib/FilterList/parameter-test.js',
-    'lib/Translator-test.js'
+    'src/Locale-test.js',
+    'src/LanguageArray-test.js',
+    'src/Namespace-test.js',
+    'src/FilterList-test.js',
+    'src/FilterList/parameter-test.js',
+    'src/Translator-test.js'
   ]);
   api.add_files([
-    'server/inject/accept-language-test.js'
+    'src/inject/accept-language-test.js'
   ], 'server');
 });
